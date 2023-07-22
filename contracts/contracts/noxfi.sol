b3dd918f09e357da28d2249e921b513a6a9ae7ea
TAEKEON GO <taekeongo@gmail.com>
2023-07-23 00:33:25 +0200

    order

--- contracts/contracts/noxfi.sol
+++ contracts/contracts/noxfi.sol
@@ -15,23 +15,41 @@ interface IVerifier {
     ) external view returns (bool);
 }
 
+interface IOrderVerifier {
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    ) external view returns (bool);
+}
+
 contract NoxFi {
     address public WETHAddr;
     address public DAIAddr;
+    address public matcherAddr;
     address public depositVerifierAddr;
     address public withdrawVerifierAddr;
+    address public orderVerifierAddr;
     uint[] public zArr;
     uint[] public nArr;
+    uint[] public oArr;
+    uint[] public mArr; // matched
+    uint[] public cArr; // canceled
+    uint[] public onArr; // order nullifier
+    uint public settlementPrice;
     // root is calculated by xor of all Zs.
     // Used for existence of Z
     // But skip it in this hackathon :P
     // uint root;
 
-    constructor(address _WETHAddr, address _DAIAddr, address _depositVerifierAddr, address _withdrawVerifierAddr) {
+    constructor(address _WETHAddr, address _DAIAddr, address _matcherAddr, address _depositVerifierAddr, address _withdrawVerifierAddr, address _orderVerifierAddr) {
         WETHAddr = _WETHAddr;
         DAIAddr = _DAIAddr;
+        matcherAddr = _matcherAddr;
         depositVerifierAddr = _depositVerifierAddr;
         withdrawVerifierAddr = _withdrawVerifierAddr;
+        orderVerifierAddr = _orderVerifierAddr;
     }
 
     function verifyDepositProof(
@@ -52,19 +70,27 @@ contract NoxFi {
         return IVerifier(withdrawVerifierAddr).verifyProof(a, b, c, input);
     }
 
+    function verifyOrderProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public view returns (bool) {
+        return IOrderVerifier(orderVerifierAddr).verifyProof(a, b, c, input);
+    }
+
     function deposit(
         uint256[2] memory a,
         uint256[2][2] memory b,
         uint256[2] memory c,
         uint256[3] memory input
-    ) external returns (bool) {
+    ) external {
         require(verifyDepositProof(a, b, c, input), "Filed proof check");
         for (uint i = 0; i < zArr.length; ++i) {
             require(zArr[i] != input[0], "The commit Z already exists");
         }
         IERC20(input[2] == 0 ? WETHAddr : DAIAddr).transferFrom(msg.sender, address(this), input[1] * 10 ** 18);
         zArr.push(input[0]);
-        return true;
     }
 
     function withdraw(
@@ -72,14 +98,58 @@ contract NoxFi {
         uint256[2][2] memory b,
         uint256[2] memory c,
         uint256[3] memory input
-    ) external returns (bool) {
+    ) external {
         require(verifyWithdrawProof(a, b, c, input), "Filed proof check");
         for (uint i = 0; i < nArr.length; ++i) {
             require(nArr[i] != input[0], "The nullifier N already exists");
         }
         IERC20(input[2] == 0 ? WETHAddr : DAIAddr).transfer(msg.sender, input[1] * 10 ** 18);
         nArr.push(input[0]);
-        return true;
+    }
+
+    function order(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) external {
+        require(verifyOrderProof(a, b, c, input), "Filed proof check");
+        for (uint i = 0; i < nArr.length; ++i) {
+            require(nArr[i] != input[0], "The nullifier N already exists");
+        }
+        // don't need to check the existence of O
+        nArr.push(input[0]);
+        oArr.push(input[1]);
+    }
+
+    function matchOrder(uint256 o, bool isMatched) external {
+      require(msg.sender == matcherAddr, "Only Matcher can make a match");
+      bool exists = false;
+      for (uint i = 0; i < oArr.length; ++i) {
+        if (oArr[i] == o) {
+          exists = true;
+          break;
+        }
+      }
+      require(exists, "The O doesn't exist");
+      for (uint i = 0; i < mArr.length; ++i) {
+        require(mArr[i] != o, "The O already matched");
+      }
+      for (uint i = 0; i < cArr.length; ++i) {
+        require(cArr[i] != o, "The O already canceled");
+      }
+      if(isMatched) {
+        mArr.push(o);
+      } else {
+        cArr.push(o);
+      }
+    }
+
+    function setSettlementPrice(uint price) external {
+      require(msg.sender == matcherAddr, "Only Matcher can set settlement price");
+      settlementPrice = price;
     }
 }
+
+
 
--- contracts/test/test.js
+++ contracts/test/test.js
@@ -3,9 +3,12 @@ const { ethers } = require("hardhat");
 const { exportCallDataGroth16 } = require("./utils/utils");
 
 describe("NoxFi", function () {
-  let depositVerifier, withdrawVerifier, noxFi, weth, dai;
+  let depositVerifier, withdrawVerifier, orderVerifier, noxFi, weth, dai;
+  let signer, signer1, signer2;
 
   before(async function () {
+    [signer, signer1, signer2] = await ethers.getSigners();
+
     depositVerifier = await ethers.deployContract("DepositVerifier", []);
     await depositVerifier.waitForDeployment();
     const depositVerifierAddr = await depositVerifier.getAddress();
@@ -14,14 +17,19 @@ describe("NoxFi", function () {
     await withdrawVerifier.waitForDeployment();
     const withdrawVerifierAddr = await withdrawVerifier.getAddress();
 
+    orderVerifier = await ethers.deployContract("OrderVerifier", []);
+    await orderVerifier.waitForDeployment();
+    const orderVerifierAddr = await orderVerifier.getAddress();
+
     weth = await ethers.deployContract("Token", ["WETH", "WETH"]);
     await weth.waitForDeployment();
     const wethAddr = await weth.getAddress();
+
     dai = await ethers.deployContract("Token", ["DAI", "DAI"]);
     await dai.waitForDeployment();
     const daiAddr = await dai.getAddress();
 
-    noxFi = await ethers.deployContract("NoxFi", [wethAddr, daiAddr, depositVerifierAddr, withdrawVerifierAddr]);
+    noxFi = await ethers.deployContract("NoxFi", [wethAddr, daiAddr, signer.address, depositVerifierAddr, withdrawVerifierAddr, orderVerifierAddr]);
     await noxFi.waitForDeployment();
     const noxFiAddr = await noxFi.getAddress();
 
@@ -112,6 +120,65 @@ describe("NoxFi", function () {
     );
     balance = await weth.balanceOf(await noxFi.getAddress());
     expect(balance).to.equal(0);
+  });
+  it("Deposit and Order Scenario", async function () {
+    const salt = 1234567890;
+    const amount = 5;
+    const asset = 1;
+
+    const input = {
+      salt: salt,
+      amount: amount,
+      asset: asset,
+    };
+
+    let dataResult = await exportCallDataGroth16(
+      input,
+      "./zkproof/deposit.wasm",
+      "./zkproof/deposit_final.zkey"
+    );
+
+    let result = await noxFi.deposit(
+      dataResult.a,
+      dataResult.b,
+      dataResult.c,
+      dataResult.Input
+    );
+    const balance = await dai.balanceOf(await noxFi.getAddress());
+    expect(balance).to.not.equal(0);
+
+    const orderInput = {
+      salt: salt,
+      amount: amount,
+      asset: asset,
+      price: 1800,
+    };
+
+    let orderDataResult = await exportCallDataGroth16(
+      orderInput,
+      "./zkproof/order.wasm",
+      "./zkproof/order_final.zkey"
+    );
+
+    await noxFi.order(
+      orderDataResult.a,
+      orderDataResult.b,
+      orderDataResult.c,
+      orderDataResult.Input
+    );
+
+    let withdrawDataResult = await exportCallDataGroth16(
+      input,
+      "./zkproof/withdraw.wasm",
+      "./zkproof/withdraw_final.zkey"
+    );
+
+    await expect(noxFi.withdraw(
+      dataResult.a,
+      dataResult.b,
+      dataResult.c,
+      dataResult.Input
+    )).to.be.reverted;
   });
 });
 
